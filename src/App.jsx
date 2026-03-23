722707b57d4faf25f896604026691a94dcaf481f
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2026-03-23 19:56:48 +0530

    Configure GitHub Pages deployment workflows and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { selectTheme } from './store/slices/uiSlice';
 import Navbar from './components/Navbar/Navbar.jsx';
